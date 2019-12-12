40375e683bf00991d217b827ab8adedbad008877
Sultanova Asel <aselft08@gmail.com>
2019-12-12 16:05:59 +0600

    Lesson - 15 add new components and better css

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+  return (
+    <div className={s.item}>
+      <img src="https://s.yimg.com/ny/api/res/1.2/7bbMUDnuJ0DeybIOk_CVzQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/68ead40c4b63a59b3c33c2e01a647f19" alt="" />
+      post 1
+      <div>
+        <span>like</span>
+      </div>
+
+    </div>
+  )
+
+}
+
+export default Post;
