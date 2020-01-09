7d2d4518a2e9b08f7f8990027fd1aedc6eb09c09
Sultanova Asel <aselft08@gmail.com>
2020-01-09 20:26:46 +0600

    l-54 life cycle

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,18 +4,12 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/user.jpg'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items);
-                });
-        }
-
-    getUsers = () => {
-
-    }
+       componentDidMount() {
+         axios.get("https://social-network.samuraijs.com/api/1.0/users")
+             .then(response => {
+                 this.props.setUsers(response.data.items);
+             });
+     }
 
     render () {
         return <div>
